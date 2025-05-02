9af5bc09ee612e1306f88b136dea834b6f80a57a
Mike S <CyberMerln@gmail.com>
2025-05-02 07:33:33 -0400

    Add  BMM150 & BMI270 test apps

---
+++ i2c_bmi270/src/main.cpp
@@ -0,0 +1,285 @@
+/*
+ * Copyright (c) 2021 Bosch Sensortec GmbH
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+
+#include <sample_usbd.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/ring_buffer.h>
+
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/usb/usbd.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/drivers/gpio.h>
+
+LOG_MODULE_REGISTER(cdc_acm_echo, LOG_LEVEL_INF);
+
+#include "UARTDevice.h"
+
+#include <zephyr/drivers/sensor.h>
+
+
+const struct device *const usb_uart_dev = DEVICE_DT_GET_ONE(zephyr_cdc_acm_uart);
+
+
+//UARTDevice SerialX(serial_dev);
+UARTDevice USBSerial(usb_uart_dev);
+
+
+inline int min(int a, int b) {
+	return (a <= b)? a : b;
+}
+
+inline void delay(uint32_t ms) {
+	k_sleep(K_MSEC(ms));
+}
+
+
+static inline void print_baudrate(const struct device *dev)
+{
+	uint32_t baudrate;
+	int ret;
+
+	ret = uart_line_ctrl_get(dev, UART_LINE_CTRL_BAUD_RATE, &baudrate);
+	if (ret) {
+		LOG_WRN("Failed to get baudrate, ret code %d", ret);
+	} else {
+		LOG_INF("Baudrate %u", baudrate);
+	}
+}
+
+#if defined(CONFIG_USB_DEVICE_STACK_NEXT)
+static struct usbd_context *sample_usbd;
+K_SEM_DEFINE(dtr_sem, 0, 1);
+
+static void sample_msg_cb(struct usbd_context *const ctx, const struct usbd_msg *msg)
+{
+	LOG_INF("USBD message: %s", usbd_msg_type_string(msg->type));
+
+	if (usbd_can_detect_vbus(ctx)) {
+		if (msg->type == USBD_MSG_VBUS_READY) {
+			if (usbd_enable(ctx)) {
+				LOG_ERR("Failed to enable device support");
+			}
+		}
+
+		if (msg->type == USBD_MSG_VBUS_REMOVED) {
+			if (usbd_disable(ctx)) {
+				LOG_ERR("Failed to disable device support");
+			}
+		}
+	}
+
+	if (msg->type == USBD_MSG_CDC_ACM_CONTROL_LINE_STATE) {
+		uint32_t dtr = 0U;
+
+		uart_line_ctrl_get(msg->dev, UART_LINE_CTRL_DTR, &dtr);
+		if (dtr) {
+			k_sem_give(&dtr_sem);
+		}
+	}
+
+	if (msg->type == USBD_MSG_CDC_ACM_LINE_CODING) {
+		print_baudrate(msg->dev);
+	}
+}
+
+static int enable_usb_device_next(void)
+{
+	int err;
+
+	sample_usbd = sample_usbd_init_device(sample_msg_cb);
+	if (sample_usbd == NULL) {
+		LOG_ERR("Failed to initialize USB device");
+		return -ENODEV;
+	}
+
+	if (!usbd_can_detect_vbus(sample_usbd)) {
+		err = usbd_enable(sample_usbd);
+		if (err) {
+			LOG_ERR("Failed to enable device support");
+			return err;
+		}
+	}
+
+	LOG_INF("USB device support enabled");
+
+	return 0;
+}
+#endif /* defined(CONFIG_USB_DEVICE_STACK_NEXT) */
+
+
+
+// Need to do this.
+unsigned long micros(void) {
+#ifdef CONFIG_TIMER_HAS_64BIT_CYCLE_COUNTER
+  return k_cyc_to_us_floor32(k_cycle_get_64());
+#else
+  return k_cyc_to_us_floor32(k_cycle_get_32());
+#endif
+ }
+
+
+
+int main(void)
+{
+  int ret;
+  
+//===============================================================	
+// BUGBUG:: need to wrap this in class...
+#if defined(CONFIG_USB_DEVICE_STACK_NEXT)
+		ret = enable_usb_device_next();
+#else
+		ret = usb_enable(NULL);
+#endif
+
+	if (ret != 0) {
+		LOG_ERR("Failed to enable USB");
+		return 0;
+	}
+
+//	ring_buf_init(&ringbuf, sizeof(ring_buffer), ring_buffer);
+
+	LOG_INF("Wait for DTR");
+
+#if defined(CONFIG_USB_DEVICE_STACK_NEXT)
+	k_sem_take(&dtr_sem, K_FOREVER);
+#else
+	while (true) {
+		uint32_t dtr = 0U;
+
+		uart_line_ctrl_get(usb_uart_dev, UART_LINE_CTRL_DTR, &dtr);
+		if (dtr) {
+			break;
+		} else {
+			/* Give CPU resources to low priority threads. */
+			k_sleep(K_MSEC(100));
+		}
+	}
+#endif
+
+	LOG_INF("DTR set");
+
+	/* They are optional, we use them to test the interrupt endpoint */
+	ret = uart_line_ctrl_set(usb_uart_dev, UART_LINE_CTRL_DCD, 1);
+	if (ret) {
+		LOG_WRN("Failed to set DCD, ret code %d", ret);
+	}
+
+	ret = uart_line_ctrl_set(usb_uart_dev, UART_LINE_CTRL_DSR, 1);
+	if (ret) {
+		LOG_WRN("Failed to set DSR, ret code %d", ret);
+	}
+
+	/* Wait 100ms for the host to do all settings */
+	k_msleep(100);
+
+#ifndef CONFIG_USB_DEVICE_STACK_NEXT
+	print_baudrate(usb_uart_dev);
+#endif
+//	uart_irq_callback_set(usb_uart_dev, interrupt_handler);
+	/* Enable rx interrupts */
+//	uart_irq_rx_enable(usb_uart_dev);
+
+	USBSerial.begin();
+	//SerialX.begin();
+
+//	USBSerial.print("Print connector pins\n");
+//  	for (size_t i = 0; i < ARRAY_SIZE(connector_pins); i++) {
+//  		USBSerial.printf("%u %p %u\n", i, connector_pins[i].port, connector_pins[i].pin);
+//  	}
+  	// Main loop, would be nice if we setup events for the two RX queues, but startof KISS
+
+  // find BMI270
+	const struct device *const dev = DEVICE_DT_GET_ONE(bosch_bmi270);
+	struct sensor_value acc[3], gyr[3];
+	struct sensor_value full_scale, sampling_freq, oversampling;
+
+	if (!device_is_ready(dev)) {
+		USBSerial.printf("BMI270 %s is not ready\n", dev->name);
+		return 0;
+	}
+
+	USBSerial.printf("BMI270 %p name is %s\n", dev, dev->name);
+
+	/* Setting scale in G, due to loss of precision if the SI unit m/s^2
+	 * is used
+	 */
+	full_scale.val1 = 2;            /* G */
+	full_scale.val2 = 0;
+	sampling_freq.val1 = 100;       /* Hz. Performance mode */
+	sampling_freq.val2 = 0;
+	oversampling.val1 = 1;          /* Normal mode */
+	oversampling.val2 = 0;
+
+	sensor_attr_set(dev, SENSOR_CHAN_ACCEL_XYZ, SENSOR_ATTR_FULL_SCALE,
+			&full_scale);
+	sensor_attr_set(dev, SENSOR_CHAN_ACCEL_XYZ, SENSOR_ATTR_OVERSAMPLING,
+			&oversampling);
+	/* Set sampling frequency last as this also sets the appropriate
+	 * power mode. If already sampling, change to 0.0Hz before changing
+	 * other attributes
+	 */
+	sensor_attr_set(dev, SENSOR_CHAN_ACCEL_XYZ,
+			SENSOR_ATTR_SAMPLING_FREQUENCY,
+			&sampling_freq);
+
+
+	/* Setting scale in degrees/s to match the sensor scale */
+	full_scale.val1 = 500;          /* dps */
+	full_scale.val2 = 0;
+	sampling_freq.val1 = 100;       /* Hz. Performance mode */
+	sampling_freq.val2 = 0;
+	oversampling.val1 = 1;          /* Normal mode */
+	oversampling.val2 = 0;
+
+	sensor_attr_set(dev, SENSOR_CHAN_GYRO_XYZ, SENSOR_ATTR_FULL_SCALE,
+			&full_scale);
+	sensor_attr_set(dev, SENSOR_CHAN_GYRO_XYZ, SENSOR_ATTR_OVERSAMPLING,
+			&oversampling);
+	/* Set sampling frequency last as this also sets the appropriate
+	 * power mode. If already sampling, change sampling frequency to
+	 * 0.0Hz before changing other attributes
+	 */
+	sensor_attr_set(dev, SENSOR_CHAN_GYRO_XYZ,
+			SENSOR_ATTR_SAMPLING_FREQUENCY,
+			&sampling_freq);
+
+	while (1) {
+		/* 10ms period, 100Hz Sampling frequency */
+		k_sleep(K_MSEC(10));
+
+		sensor_sample_fetch(dev);
+
+		sensor_channel_get(dev, SENSOR_CHAN_ACCEL_XYZ, acc);
+		sensor_channel_get(dev, SENSOR_CHAN_GYRO_XYZ, gyr);
+    
+/**
+ * @brief Representation of a sensor readout value.
+ *  extracted from sensor.h
+ * The value is represented as having an integer and a fractional part,
+ * and can be obtained using the formula val1 + val2 * 10^(-6). Negative
+ * values also adhere to the above formula, but may need special attention.
+ * Here are some examples of the value representation:
+ *
+ *      0.5: val1 =  0, val2 =  500000
+ *     -0.5: val1 =  0, val2 = -500000
+ *     -1.0: val1 = -1, val2 =  0
+ *     -1.5: val1 = -1, val2 = -500000
+ */
+    float accel[3], gyro[3];
+    for(uint8_t i = 0; i < 3; i++) {
+      accel[i] = (float)acc[i].val1 + (float)acc[i].val2 / 1000000;
+      gyro[i] =  (float)gyr[i].val1 + (float)gyr[i].val2 / 1000000;
+    }
+    USBSerial.printf("AX: %.6f; AY: %.6f; AZ: %.6f; GX: %.6f; GY: %.6f; GZ: %.6f;\n", accel[0], accel[1],accel[2], gyro[0],gyro[1], gyro[2]);
+	}
+	return 0;
+}
