795297a276ddb9f75fc9b1e3f53cae1435a489fc
kurte <kurte@rockisland.com>
2025-07-14 16:55:11 -0700

    SDRam test...

---
+++ SDRAM_test/README.rst
@@ -0,0 +1,21 @@
+.. zephyr:code-sample:: usb-cdc-acm
+   :name: USB CDC-ACM
+   :relevant-api: usbd_api _usb_device_core_api uart_interface
+
+   Use USB CDC-ACM driver to implement a serial port echo.
+
+Overview
+********
+
+This sample app demonstrates use of a USB Communication Device Class (CDC)
+
+Requirements
+************
+
+
+Building and Running
+********************
+
+Reel Board
+===========
+
---
+++ SDRAM_test/src/main.cpp
@@ -0,0 +1,197 @@
+/*
+ * Copyright (c) 2019 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Sample echo app for CDC ACM class
+ *
+ * Sample app for USB CDC ACM class driver. The received data is echoed back
+ * to the serial port.
+ */
+
+//#include <sample_usbd.h>
+// Turn on this option, to test to ee if camera errors out without screeen
+//#define TIMED_WAIT_NO_TFT (1000/6)
+
+// Try using fixed normal memory buffer for display
+//#define ILI9341_USE_FIXED_BUFFER
+// Hack try to use fixed buffer for camera
+#include <stdio.h>
+#include <string.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/ring_buffer.h>
+#include <zephyr/drivers/clock_control.h>
+
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/usb/usbd.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/input/input.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+#include <zephyr/devicetree.h>
+#include <zephyr/multi_heap/shared_multi_heap.h>
+#include <zephyr/drivers/dma.h>
+
+inline int min(int a, int b) {
+	return (a <= b)? a : b;
+}
+
+inline void delay(uint32_t ms) {
+	k_sleep(K_MSEC(ms));
+}
+
+// Need to do this.
+unsigned long micros(void) {
+#ifdef CONFIG_TIMER_HAS_64BIT_CYCLE_COUNTER
+  return k_cyc_to_us_floor32(k_cycle_get_64());
+#else
+  return k_cyc_to_us_floor32(k_cycle_get_32());
+#endif
+ }
+
+
+uint8_t *sdram_buffers[3];
+//#define BUFFER_SIZE (320*240*2)
+#define BUFFER_SIZE (32768)
+volatile bool dma_completed;
+
+void dma_callback(const struct device *dma_dev, void *arg, uint32_t channel, int status)
+{
+    if (status == 0) {
+//        LOG_INF("DMA transfer complete on channel %d", channel);
+    } else {
+        LOG_ERR("DMA transfer failed with status %d on channel %d", status, channel);
+    }
+    dma_completed = true;
+}
+
+int configure_dma_transfer(const struct device *dma_dev, uint8_t *src, uint8_t *dest, size_t size, uint8_t channel)
+{
+	struct dma_config dma_cfg = {0};
+	struct dma_block_config block_cfg = {0};
+
+	block_cfg.source_address = (uint32_t)src;
+	block_cfg.dest_address = (uint32_t)dest;
+	block_cfg.block_size = size;
+
+	LOG_DBG("source: 0x%x, dest: 0x%x", block_cfg.source_address, block_cfg.dest_address);
+
+	dma_cfg.channel_direction = MEMORY_TO_MEMORY;
+	dma_cfg.source_burst_length = 4;  // Assuming 4 bytes per burst
+	dma_cfg.dest_burst_length = 4;
+	dma_cfg.source_data_size = 4;  // 4 bytes (32 bits)
+	dma_cfg.dest_data_size = 4;
+	dma_cfg.block_count = 1;
+	dma_cfg.head_block = &block_cfg;
+	dma_cfg.dma_callback = dma_callback;
+
+//	LOG_INF("Configuring DMA...");
+
+	int ret = dma_config(dma_dev, channel, &dma_cfg);
+	if (ret != 0) {
+		LOG_ERR("DMA configuration failed with error %d\n", ret);
+		return ret;
+	}
+
+//	LOG_INF("DMA configured");
+
+    dma_completed = false;
+	ret = dma_start(dma_dev, channel);
+	if (ret != 0) {
+		LOG_ERR("Failed to start DMA transfer with error %d\n", ret);
+	}
+
+//	LOG_INF("DMA started");
+
+	return ret;
+}
+
+
+int CheckBuffers(const char *sz, uint8_t *source, uint8_t *dest) {
+	int error_count = 0;
+	for (int i = 0; i < BUFFER_SIZE; i++) {
+		if (source[i] != dest[i]) {
+			error_count++;
+			if (error_count == 1) printk("%s ", sz);
+			if (error_count < 5) printk(" (%i %x %x)", i, source[i], dest[i]);
+		}
+	}
+	if (error_count) printk(" %u", error_count);
+	return error_count;
+}
+
+
+int main(void)
+{
+
+	const struct device *dma_dev = DEVICE_DT_GET(DT_ALIAS(dmax));
+	struct dma_status dma_status = {0};
+	int ret = 0;
+	uint8_t tx_dma_channel = 2;
+	// Start up the Serial
+	//SerialX.begin();
+	delay(250);
+	printk("SDRAM test...\n");
+	for (uint8_t i = 0; i < (sizeof(sdram_buffers) / sizeof(sdram_buffers[0])); i++) {
+		sdram_buffers[i] = (uint8_t *)shared_multi_heap_aligned_alloc(SMH_REG_ATTR_EXTERNAL, 32, BUFFER_SIZE);
+		printk("Buffer: %u ADDR:%p\n", i, sdram_buffers[i]);
+	}
+
+	for (uint32_t loop_count = 0;; loop_count++) {
+		printk("loop: %u(%x) ", loop_count, loop_count & 0xff);
+		memset (sdram_buffers[0], loop_count & 0xff, BUFFER_SIZE);
+
+		ret = configure_dma_transfer(dma_dev, sdram_buffers[0], sdram_buffers[2], BUFFER_SIZE, tx_dma_channel);
+		if (ret != 0) {
+			LOG_ERR("DMA configuration or transfer failed\n");
+		}
+
+		memcpy(sdram_buffers[1], sdram_buffers[0], BUFFER_SIZE);
+
+		CheckBuffers(" Memcpy:", sdram_buffers[0], sdram_buffers[1]);
+		while (!dma_completed) delay(1);
+		CheckBuffers(" DMA:", sdram_buffers[0], sdram_buffers[2]);
+		printk("\n");
+		delay(259);
+	}
+	return 0;
+}
+
+//----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
+extern unsigned long millis(void);
+
+unsigned long millis(void) { return k_uptime_get_32(); }
+
+//----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
+
+__stm32_sdram1_section static uint8_t __aligned(32) smh_pool[4*1024*1024];
+
+int smh_init(void) {
+    int ret = 0;
+    ret = shared_multi_heap_pool_init();
+    if (ret != 0) {
+        return ret;
+    }
+
+    struct shared_multi_heap_region smh_sdram = {
+        .attr = SMH_REG_ATTR_EXTERNAL,
+        .addr = (uintptr_t) smh_pool,
+        .size = sizeof(smh_pool)
+    };
+
+    ret = shared_multi_heap_add(&smh_sdram, NULL);
+    if (ret != 0) {
+        return ret;
+    }
+	return 0;
+}
+
+SYS_INIT(smh_init, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
